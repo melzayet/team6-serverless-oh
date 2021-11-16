458cbfc8d9151d25b1fff7d9d41ae1220c090b39
Mahmoud El Zayet <38279473+melzayet@users.noreply.github.com>
2021-11-16 14:47:18 +0000

    added dto rating class

---
+++ dto/rating.cs
@@ -0,0 +1,10 @@
+public class Rating
+{
+    public string id { get; set; }
+    public string userId { get; set; }
+    public string productId { get; set; }
+    public string timestamp { get; set; }
+    public string locationName { get; set; }
+    public int rating { get; set; }
+    public string userNotes { get; set; }
+}
