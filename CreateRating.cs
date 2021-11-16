c5c5fdbe936c5ead591c9a3d33f303b0bd62556c
Mahmoud El Zayet <38279473+melzayet@users.noreply.github.com>
2021-11-16 14:44:43 +0000

    validated input

--- CreateRating.cs
+++ CreateRating.cs
@@ -20,9 +20,31 @@ namespace IceCream.Rating
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string userId = await GetUserClient.GetUser("46cfc75f-5942-4426-8bf5-b960845223d1");
-            Trace.WriteLine(userId);
-            return new OkObjectResult(userId);
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            string userId = "cc20a6fb-a91f-4192-874d-132493685376" ?? data?.userId;
+            string productId = "75542e38-563f-436f-adeb-f426f1dabb5c" ?? data?.productId;
+            string rating = "75542e38-563f-436f-adeb-f426f1dabb5c" ?? data?.rating;
+            string responseString = "success";
+
+            //validate user
+            string userCheckResult = await GetUserClient.GetUser(userId);
+            dynamic userObject = JsonConvert.DeserializeObject(userCheckResult);
+            if(userObject?.userId != userId)  responseString = "invalid user Id";
+            Trace.WriteLine(userCheckResult);            
+
+            //validate product
+            string productCheckResult = await GetProductClient.GetProduct(productId);
+            dynamic productObject = JsonConvert.DeserializeObject(productCheckResult);
+            if(productObject?.productId != productId)  responseString = "invalid product Id";
+            Trace.WriteLine(productCheckResult);
+
+             //validate raring    (need check if it's a number)
+            if(Int32.Parse(rating) < 0 || Int32.Parse(rating) > 5)  responseString = "invalid rating";
+            Trace.WriteLine(userCheckResult); 
+
+            return new OkObjectResult(responseString);
         }
     }
 }
