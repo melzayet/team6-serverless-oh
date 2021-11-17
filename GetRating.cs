ef12b0268379b951408351a3a9368103597c753f
Mahmoud El Zayet <38279473+melzayet@users.noreply.github.com>
2021-11-17 11:07:04 +0000

    update rating route

--- GetRating.cs
+++ GetRating.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Microsoft.Azure.WebJobs.Host;
+using System.Linq;
+using System.Collections.Generic;
 
 
 
@@ -23,15 +25,20 @@ namespace IceCream.Rating
                 databaseName: "BFYOC_Datastore",
                 collectionName: "Ratings",
                 ConnectionStringSetting = "CosmosDBSetting",
-                SqlQuery = "SELECT * FROM c WHERE c.id = {ratingId}")] dto.Rating rating,
+                SqlQuery = "SELECT * FROM c WHERE c.id = {ratingId}")] IEnumerable<dto.Rating> ratings,
             ILogger log)
         {
 
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            if(rating == null)
-                return new NotFoundObjectResult("rating does not exist");
-            else return new OkObjectResult(rating);
+             if(!ratings.Any())
+                {                    
+                    return new NotFoundObjectResult("No rating found");
+                }
+            else 
+                {                 
+                    return new OkObjectResult(ratings);
+                }
 
 
         }
