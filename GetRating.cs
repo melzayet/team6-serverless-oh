6de7ddcdbe77aa500fd0671370b4d7c3796a4492
Joel Hebert <joel.hebert@opulentasp.com>
2021-11-17 10:36:33 +0000

    Update GetRating.cs

--- GetRating.cs
+++ GetRating.cs
@@ -23,7 +23,7 @@ namespace IceCream.Rating
                 databaseName: "BFYOC_Datastore",
                 collectionName: "Ratings",
                 ConnectionStringSetting = "CosmosDBSetting",
-                Id = "{Query.ratingId}"] dto.Rating rating,
+                Id = "{Query.ratingId}")] dto.Rating rating,
             ILogger log)
         {
 
